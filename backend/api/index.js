f45ba840fe5272e796aebb55f8f723ab92c61239
Abdulrhaman-Ahmed <ikarzma37@gmail.com>
2026-04-02 17:31:39 +0200

    deploy backend to vercel

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,17 +1,3 @@
-import express from 'express';
-import cors from 'cors';
-
-import productRoutes from '../routes/products.js';
-import orderRoutes from '../routes/orders.js';
-import categoryRoutes from '../routes/categories.js';
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-app.use('/api/products', productRoutes);
-app.use('/api/orders', orderRoutes);
-app.use('/api/categories', categoryRoutes);
+import app from '../server.js'; 
 
 export default app;
--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,6 @@ import categoryRoutes from './routes/categories.js';
 import orderRoutes from './routes/orders.js';
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
@@ -31,6 +30,6 @@ app.use((req, res) => {
   res.status(404).json({ message: 'Route not found' });
 });
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
+
+
+export default app;
